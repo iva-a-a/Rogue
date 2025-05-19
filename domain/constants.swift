cf4b6131b7348f2ba6df3e25c8883c9e05d26186
satvajos <satvajos@student.21-school.ru>
2025-05-19 16:07:05 +0300

    move common struct and const

---
+++ domain/constants.swift
@@ -0,0 +1,30 @@
+//
+//  constants.swift
+//  rogue
+
+enum Constants {
+    enum Map {
+        static let height: Int = 24
+        static let width: Int = 80
+    }
+    enum Grid {
+        static let size: Int = 3
+        static let width: Int = Map.width / size
+        static let height: Int = Map.height / size
+    }
+
+    enum Room {
+        static let minWidth: Int = 6
+        static let minHeight: Int = 5
+
+        static let maxWidth: Int = 10
+        static let maxHeight: Int = 7
+    }
+
+    enum Graph {
+        static let countNode: Int = 9
+    }
+
+    static let indent: Int = 1
+    static let doorOffset: Int = 2
+}
