eb9560bb7f7b7925660ecb931f0c88aad05e64d3
Magomet Bekov <magafif57@gmail.com>
2025-06-05 00:45:15 +0300

    Changing init to public

--- domain/position.swift
+++ domain/position.swift
@@ -6,7 +6,7 @@ public struct Position: Hashable, Equatable{
     public let x: Int
     public let y: Int
 
-    init(_ x: Int, _ y: Int) {
+    public init(_ x: Int, _ y: Int) {
         self.x = x
         self.y = y
     }
