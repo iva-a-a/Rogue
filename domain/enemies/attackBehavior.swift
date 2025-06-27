398546a0cc316ae8e8c6eb189487dc01820ec9af
satvajos <satvajos@student.21-school.ru>
2025-06-27 17:06:04 +0300

    удалила ненужный класс

--- domain/enemies/attackBehavior.swift
+++ domain/enemies/attackBehavior.swift
@@ -17,22 +17,11 @@ class DefaultAttack: AttackBehavior {
     }
 }
 
-class FirstMissAttack: DefaultAttack {
-    private var didMiss = false
-    override func attack(attacker: Enemy, player: Player) -> AttackResult {
-        if !didMiss {
-            didMiss = true
-            return .miss
-        }
-        return super.attack(attacker: attacker, player: player)
-    }
-}
-
 class DrainHealthAttack: DefaultAttack {
     override func attack(attacker: Enemy, player: Player) -> AttackResult {
         let result = super.attack(attacker: attacker, player: player)
         if case .hit = result {
-            player.characteristics.maxHealth -= 5
+            player.characteristics.maxHealth -= Range(1...5).randomElement() ?? 3
             player.characteristics.health = min(player.characteristics.health, player.characteristics.maxHealth)
         }
         return result
