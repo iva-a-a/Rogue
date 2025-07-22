ea8a18f37b9b34e3de02f16c1bd5eff5febf1dea
satvajos <satvajos@student.21-school.ru>
2025-07-22 15:01:58 +0300

    добавила модели для сохранения игры

--- data/dataModels.swift
+++ data/dataModels.swift
@@ -23,7 +23,10 @@ public struct GameAttempt: Codable {
     public var tilesExplored: Int
     //public let namePlayer: String
     
-    public init(date: Date, levelsCompleted: Int, totalTreasure: Int, finalScore: Int, wasSuccessful: Bool, playTime: TimeInterval, enemiesDefeated: Int, foodConsumed: Int, elixirsDrunk: Int, scrollsRead: Int, attacksMade: Int, attacksMissed: Int, missFromAttack: Int, hitFromAttack: Int, tilesExplored: Int) {
+    public init(date: Date, levelsCompleted: Int, totalTreasure: Int,
+                finalScore: Int, wasSuccessful: Bool, playTime: TimeInterval,
+                enemiesDefeated: Int, foodConsumed: Int, elixirsDrunk: Int,
+                scrollsRead: Int, attacksMade: Int, attacksMissed: Int, missFromAttack: Int, hitFromAttack: Int, tilesExplored: Int) {
         self.date = date
         self.levelsCompleted = levelsCompleted
         self.totalTreasure = totalTreasure
@@ -42,55 +45,259 @@ public struct GameAttempt: Codable {
     }
 }
 
-public struct Leaderboard: Codable {
-    let attempts: [GameAttempt]
+public struct LevelDTO: Codable {
+    public let rooms: [RoomDTO]
+    public let corridors: [CorridorDTO]
+    public let exitPosition: PositionDTO
+    public let player: PlayerDTO
+    public let enemies: [EnemyDTO]
+    public let items: [PositionDTO: ItemDTO]
+    public let levelNumber: Int
+    public let gameMap: GameMapDTO
+    
+    public init(rooms: [RoomDTO],
+                corridors: [CorridorDTO],
+                exitPosition: PositionDTO,
+                player: PlayerDTO,
+                enemies: [EnemyDTO],
+                items: [PositionDTO: ItemDTO],
+                levelNumber: Int,
+                gameMap: GameMapDTO) {
+        self.rooms = rooms
+        self.corridors = corridors
+        self.exitPosition = exitPosition
+        self.player = player
+        self.enemies = enemies
+        self.items = items
+        self.levelNumber = levelNumber
+        self.gameMap = gameMap
+    }
 }
 
-
-/*
-// Модель для сохранения состояния игрока
-public struct PlayerSaveData: Codable {
-    let characteristics: Characteristics
-    let inventory: [InventoryItemSaveData] // Используем InventoryItemSaveData для конкретных предметов
-    let score: Int
-    let currentLevel: Int
+public struct RoomDTO: Codable {
+    public let topRight: PositionDTO
+    public let lowLeft: PositionDTO
+    public let doors: [DoorDTO]
+    
+    public init(topRight: PositionDTO, lowLeft: PositionDTO, doors: [DoorDTO]) {
+        self.topRight = topRight
+        self.lowLeft = lowLeft
+        self.doors = doors
+    }
 }
 
-// Модель для элемента инвентаря игрока
-public struct InventoryItemSaveData: Codable {
-    let category: ItemCategory
-    let specificType: String // Уникальный идентификатор для каждого типа предмета
+public struct CorridorDTO: Codable {
+    public let route: [PositionDTO]
+    
+    public init(route: [PositionDTO]) {
+        self.route = route
+    }
 }
 
-// Модель для сохранения состояния врага
-public struct EnemySaveData: Codable {
-    let type: EnemyType
-    let characteristics: Characteristics
-    let position: Position
-    let indexRoom: Int
-    let isVisible: Bool
+public struct PositionDTO: Codable, Hashable {
+    public let x: Int
+    public let y: Int
+    
+    public init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
 }
 
-// Модель для сохранения состояния предмета на уровне
-public struct ItemSaveData: Codable {
-    let type: ItemCategory
-    let position: Position
+public struct DoorDTO: Codable {
+    public let position: PositionDTO
+    public let direction: DirectionDTO
+    public let color: ColorDTO
+    public let isUnlocked: Bool
+    
+    public init(position: PositionDTO, direction: DirectionDTO, color: ColorDTO, isUnlocked: Bool) {
+        self.position = position
+        self.direction = direction
+        self.color = color
+        self.isUnlocked = isUnlocked
+    }
 }
 
-// Модель для сохранения полного состояния уровня
-public struct LevelSaveData: Codable {
-    let levelNumber: Int
-    let playerData: PlayerSaveData
-    let enemies: [EnemySaveData]
-    let items: [ItemSaveData]
-    let exitPosition: Position
+public enum DirectionDTO: String, Codable, CaseIterable {
+    case up, down, left, right
 }
 
-// Модель для таблицы лидеров
-public struct LeaderboardEntry: Codable {
-    let playerName: String
-    let score: Int
-    let levelsCompleted: Int
-    let date: Date
+public enum ColorDTO: String, Codable {
+    case red, green, blue, none
 }
-*/
+
+public struct PlayerDTO: Codable {
+    public let characteristics: CharacteristicsDTO
+    public let backpack: BackpackDTO
+    public let weapon: ItemDTO?
+    public let buffs: BuffsDTO
+    public let isAsleep: Bool
+    
+    public init(characteristics: CharacteristicsDTO, backpack: BackpackDTO, weapon: ItemDTO? = nil, buffs: BuffsDTO, isAsleep: Bool) {
+        self.characteristics = characteristics
+        self.backpack = backpack
+        self.weapon = weapon
+        self.buffs = buffs
+        self.isAsleep = isAsleep
+    }
+}
+
+public struct CharacteristicsDTO: Codable {
+    public var position: PositionDTO
+    public var maxHealth: Int
+    public var health: Int
+    public var agility: Int
+    public var strength: Int
+    
+    public init(position: PositionDTO, maxHealth: Int, health: Int, agility: Int, strength: Int) {
+        self.position = position
+        self.maxHealth = maxHealth
+        self.health = health
+        self.agility = agility
+        self.strength = strength
+    }
+}
+
+public struct BackpackDTO: Codable {
+    public var items: [ItemCategoryDTO: [ItemDTO]]
+    public var totalTreasureValue: Int
+    
+    public init(items: [ItemCategoryDTO : [ItemDTO]], totalTreasureValue: Int) {
+        self.items = items
+        self.totalTreasureValue = totalTreasureValue
+    }
+}
+
+public struct ItemDTO: Codable {
+    public let type: ItemTypeDTO
+    public let value: Int?
+    public let duration: TimeInterval?
+    public let color: ColorDTO?
+    public let damage: Int?
+    
+    public init(type: ItemTypeDTO, value: Int? = nil, duration: TimeInterval? = nil, color: ColorDTO? = nil, damage: Int? = nil) {
+        self.type = type
+        self.value = value
+        self.duration = duration
+        self.color = color
+        self.damage = damage
+    }
+    
+    public init() {
+        self.type = .food(.apple)
+        self.value = nil
+        self.duration = nil
+        self.color = nil
+        self.damage = nil
+    }
+}
+
+public enum ItemTypeDTO: Codable, Hashable {
+    case food(FoodTypeDTO)
+    case weapon(WeaponTypeDTO)
+    case scroll(ScrollTypeDTO)
+    case elixir(ElixirTypeDTO)
+    case treasure(TreasureTypeDTO)
+    case key(ColorDTO)
+}
+
+public enum ItemCategoryDTO: String, Codable, Hashable {
+    case food
+    case weapon
+    case scroll
+    case elixir
+    case treasure
+    case key
+}
+
+public enum FoodTypeDTO: String, Codable, Hashable {
+    case apple, bread, meat
+}
+
+public enum ElixirTypeDTO: String, Codable, Hashable {
+    case health, agility, strength
+}
+
+public enum ScrollTypeDTO: String, Codable, Hashable {
+    case health, agility, strength
+}
+
+public enum WeaponTypeDTO: String, Codable, Hashable {
+    case sword, bow, dagger, staff
+}
+
+public enum TreasureTypeDTO: String, Codable, Hashable {
+    case gold, gem, artifact
+}
+
+public struct BuffsDTO: Codable {
+    public let health: [ElixirBufDTO]
+    public let agility: [ElixirBufDTO]
+    public let strength: [ElixirBufDTO]
+    
+    public init(health: [ElixirBufDTO], agility: [ElixirBufDTO], strength: [ElixirBufDTO]) {
+        self.health = health
+        self.agility = agility
+        self.strength = strength
+    }
+}
+
+public struct ElixirBufDTO: Codable {
+    public let statIncrease: Int
+    public let effectEnd: TimeInterval
+    
+    public init(statIncrease: Int, effectEnd: TimeInterval) {
+        self.statIncrease = statIncrease
+        self.effectEnd = effectEnd
+    }
+}
+
+public struct EnemyDTO: Codable {
+    public let type: EnemyTypeDTO
+    public let characteristics: CharacteristicsDTO
+    public let hostility: Int
+    public let isVisible: Bool
+    public let indexRoom: Int
+    public let isResting: Bool?
+    public let wasFirstAttacked: Bool?
+    public let depictsItem: Bool?
+    public let disguisedItemType: ItemTypeDTO?
+
+    public init(type: EnemyTypeDTO,
+                characteristics: CharacteristicsDTO,
+                hostility: Int,
+                isVisible: Bool,
+                indexRoom: Int,
+                isResting: Bool? = nil,
+                wasFirstAttacked: Bool? = nil,
+                depictsItem: Bool? = nil,
+                disguisedItemType: ItemTypeDTO? = nil) {
+        self.type = type
+        self.characteristics = characteristics
+        self.hostility = hostility
+        self.isVisible = isVisible
+        self.indexRoom = indexRoom
+        self.isResting = isResting
+        self.wasFirstAttacked = wasFirstAttacked
+        self.depictsItem = depictsItem
+        self.disguisedItemType = disguisedItemType
+    }
+}
+
+public enum EnemyTypeDTO: String, Codable {
+    case zombie, vampire, ghost, ogre, snakeMage, mimic
+}
+
+public struct GameMapDTO: Codable {
+    public let walkablePositions: Set<PositionDTO>
+    public let visibleTiles: Set<PositionDTO>
+    public let seenTiles: Set<PositionDTO>
+    
+    public init(walkablePositions: Set<PositionDTO>,
+                visibleTiles: Set<PositionDTO>,
+                seenTiles: Set<PositionDTO>) {
+        self.walkablePositions = walkablePositions
+        self.visibleTiles = visibleTiles
+        self.seenTiles = seenTiles
+    }
+}
