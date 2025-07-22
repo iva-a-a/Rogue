a9950c084e1fb82abb481914aa8d518d2403469b
satvajos <satvajos@student.21-school.ru>
2025-07-22 15:10:53 +0300

    первая попытка сделать сохранение и загрузку игры

--- controller/controller.swift
+++ controller/controller.swift
@@ -5,18 +5,26 @@
 import Foundation
 import domain
 import presentation
+import data
+import services
 
 public class Controller {
     private var level: Level?
     public var state: GameState = .beginning
     private var inventoryCategory: ItemCategory? = nil
     private var levelNumber: Int = 0
-    private let gameRender = GameRenderer()
+
     private let statsTracker = GameStatsTracker()
     private var lastUpdateTime = Date()
-
+    private let dataLayer = DataLayer()
+    
+    private let menu = MenuRender()
+    private let gameRender = GameRenderer()
+    private let leaderboard = LeaderboardRenderer()
+    
     public init() {
         GameEventManager.shared.addObserver(GameLogger.shared)
+        GameEventManager.shared.addObserver(self)
     }
 
     public func update(for input: PlayerAction) {
@@ -30,7 +38,14 @@ public class Controller {
     private func updateState(for input: PlayerAction) {
         switch state {
         case .beginning:
-            if input == .start { state = .generating }
+            if let action = menu.handleInput(input) {
+                switch action {
+                case .start: state = .generating
+                case .load: state = .loading
+                case .leadboard: state = .showLeaderboard
+                case .exit: state = .quit
+                }
+            }
         case .generating: state = .playing
         case .playing:
             switch input {
@@ -47,7 +62,10 @@ public class Controller {
             case .exit: state = .quit
             default: break
             }
-        case .quit: break
+        case .loading: state = .playing
+        case .showLeaderboard:
+            if input == .exit { state = .beginning }
+        case .quit: state = .beginning
         }
     }
 
@@ -65,6 +83,7 @@ public class Controller {
             default: break
             }
         case (.inventory, .playing): inventoryCategory = nil
+        case (.beginning, .showLeaderboard):  leaderboard.resetOffset()
         default: break
         }
     }
@@ -76,18 +95,22 @@ public class Controller {
             generateLevel()
         case .playing:
             motion(input)
-        case .beginning, .levelComplete, .quit:
+        case .beginning, .levelComplete:
             break
         case .inventory:
             itemAction(input)
         case .won, .lose:
             level?.player.deleteActiveBuffs()
+        case .loading:
+            GameEventManager.shared.notify(.loadGame)
+        case .showLeaderboard:
+            leaderboard.handleInput(input)
+        case .quit: saveGame()
         }
     }
     
     private func generateLevel() {
         let player = levelNumber == 1 ? Player() : (level?.player ?? Player())
-        player.deleteAllKeys()
         self.level = LevelBuilder.buildLevel(player: player, levelNumber: levelNumber)
     }
 
@@ -137,15 +160,55 @@ public class Controller {
     }
 
     public func rendering() {
-        guard let level = level else { return }
-
-        if state == .inventory, let category = inventoryCategory {
-            gameRender.renderInventory(category: inventoryCategory!,
-                                       items: level.getItemsList(category),
-                                       player: level.player)
-            return
+        switch state {
+        case .beginning: menu.render()
+        case .showLeaderboard: leaderboard.render(attempts: MapperLeaderboard.toViewModel(dataLayer.getSortedGameAttempts()))
+        case .inventory:
+            guard let level = level, let category = inventoryCategory else { return }
+            let inventoryView = ViewModelBuilder.buildInventoryViewModel(category: category,
+                                                                         items: level.getItemsList(category),
+                                                                         player: level.player)
+            gameRender.renderInventory(inventoryView)
+        default:
+            guard let level = level else { return }
+            let gameView = ViewModelBuilder.buildGameScreen(from: level)
+            gameRender.renderGameScreen(gameView)
+            GameLogger.shared.clearCombatActionLog()
         }
-        gameRender.renderLevel(level)
-        GameLogger.shared.clearCombatActionLog()
+    }
+    
+    private func saveGame() {
+        guard let level = level else { return }
+        let dto = LevelMapper.toDTO(level)
+        do {
+            try dataLayer.saveLevelDTO(dto)
+        } catch {
+
+        }
+    }
+
+    private func loadGame() {
+        do {
+            if let dto = try dataLayer.loadLevelDTO() {
+                self.level = LevelMapper.toDomain(dto)
+                self.levelNumber = dto.levelNumber
+                self.state = .playing
+            }
+        } catch {
+           // GameLogger.shared.didReceiveEvent(event: .notSaveStats)
+        }
+    }
+}
+
+extension Controller: GameEventObserver {
+    public func didReceiveEvent(event: GameEvent) {
+        switch event {
+        case .saveGame:
+            saveGame()
+        case .loadGame:
+            loadGame()
+        default:
+            break
+        }
     }
 }
