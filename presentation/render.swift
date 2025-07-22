7a41b9c794ab06c9d7d1c5e2a42846500d1d80df
satvajos <satvajos@student.21-school.ru>
2025-07-22 15:09:04 +0300

    исправила рендер

--- presentation/render.swift
+++ presentation/render.swift
@@ -5,15 +5,15 @@
 import Darwin.ncurses
 
 public class Render {
-    public init() {}
+    init() {}
 
-    public func drawTiles(_ tiles: [DrawableObject]) {
+    public static func drawTiles(_ tiles: [DrawableObject]) {
         for tile in tiles where tile.isVisible {
             tile.draw()
         }
     }
 
-    public func drawString(_ string: String, atY y: Int, atX x: Int) {
+    public static func drawString(_ string: String, atY y: Int, atX x: Int) {
         move(Int32(y), Int32(x))
         addstr(string)
     }
